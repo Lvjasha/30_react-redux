52536274608337687ce1a4f9e8c6170e97f123a5
Anastasy T <ekarniymamay@mail.ru>
2025-02-28 19:53:46 +0300

    BookForm BookList Filter

---
+++ frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,8 @@
+export const BookForm = () => {
+    return (
+        <div className="app-block book-form">
+            <h2>Add a new Book</h2>
+        </div>
+    );
+};
+export default BookForm;
