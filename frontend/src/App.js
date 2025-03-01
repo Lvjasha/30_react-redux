d4193ccd31b723479aac7b25754a270b489ff754
Anastasy T <ekarniymamay@mail.ru>
2025-03-01 12:08:33 +0300

    useSelector

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import BookForm from './components/BookForm/BookForm';
 import BookList from './components/BookList/BookList';
-import Filter from './components/Filter/Filter';
+// import Filter from './components/Filter/Filter';
 import './App.css';
 
 function App() {
--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,15 +1,24 @@
 import './BookForm.css';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/actionCreators';
 
 export const BookForm = () => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
+    const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if (title && author) {
-            console.log(title, author);
+            const book = {
+                title,
+                author,
+            };
+
+            dispatch(addBook(book));
+
             setTitle('');
             setAuthor('');
         }
@@ -18,7 +27,7 @@ export const BookForm = () => {
     return (
         <div className="app-block book-form">
             <h2>Add a new Book</h2>
-            <form onSabmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="title">Title:</label>
                     <input
