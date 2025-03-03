30b294669f5437a5cb84d97a6b370f3ddd18515d
Anastasy T <ekarniymamay@mail.ru>
2025-03-03 11:21:19 +0300

    thunk Function

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,74 +1,68 @@
 import './BookForm.css';
-import axios from 'axios';
+
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../../redux/slices/booksSlice';
 import createBookWithID from '../../utils/createBookWithID';
 import booksData from '../../data/books.json';
+import { thunkFunction } from '../../redux/slices/booksSlice';
 
 const BookForm = () => {
-    const [title, setTitle] = useState('');
-    const [author, setAuthor] = useState('');
-    const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
 
-    const handleAddRandomBook = () => {
-        const randomIndex = Math.floor(Math.random() * booksData.length);
-        const randomBook = booksData[randomIndex];
-        dispatch(addBook(createBookWithID(randomBook, 'random')));
-    };
+  const handleAddRandomBook = () => {
+    const randomIndex = Math.floor(Math.random() * booksData.length);
+    const randomBook = booksData[randomIndex];
+    dispatch(addBook(createBookWithID(randomBook, 'random')));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if (title && author) {
-            dispatch(addBook(createBookWithID({ title, author }, 'manual')));
-            setTitle('');
-            setAuthor('');
-        }
-    };
+    if (title && author) {
+      dispatch(addBook(createBookWithID({ title, author }, 'manual')));
+      setTitle('');
+      setAuthor('');
+    }
+  };
 
-    const handleAddRandomBookViaAPI = async () => {
-        try {
-            const res = await axios.get('http://localhost:4000/random-book');
-            if (res?.data?.title && res?.data?.author) {
-                dispatch(addBook(createBookWithID(res.data, 'API')));
-            }
-        } catch (error) {
-            console.log('Error fetching random book', error);
-        }
-    };
+  const handleAddRandomBookViaAPI = () => {
+    dispatch(thunkFunction);
+  };
 
-    return (
-        <div className="app-block book-form">
-            <h2>Add a new Book</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="title">Title:</label>
-                    <input
-                        type="text"
-                        id="title"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="author">Author:</label>
-                    <input
-                        type="text"
-                        id="author"
-                        value={author}
-                        onChange={(e) => setAuthor(e.target.value)}
-                    />
-                    <button type="submit">Add Book</button>
-                    <button type="button" onClick={handleAddRandomBook}>
-                        Add random book
-                    </button>
-                    <button type="button" onClick={handleAddRandomBookViaAPI}>
-                        Add random via API
-                    </button>
-                </div>
-            </form>
+  return (
+    <div className="app-block book-form">
+      <h2>Add a new Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-    );
+        <div>
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <button type="submit">Add Book</button>
+          <button type="button" onClick={handleAddRandomBook}>
+            Add random book
+          </button>
+          <button type="button" onClick={handleAddRandomBookViaAPI}>
+            Add random via API
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default BookForm;
--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,39 +1,53 @@
 import { createSlice } from '@reduxjs/toolkit';
+import createBookWithID from '../../utils/createBookWithID';
+import axios from 'axios';
 
 const initialState = [];
 
 const booksSlice = createSlice({
-    name: 'books',
-    initialState,
-    reducers: {
-        addBook: (state, action) => {
-            // return [...state, action.id];
-            state.push(action.payload);
-        },
-        deleteBook: (state, action) => {
-            // то же самое, но много текста
-            // const index = state.findIndex((book)=>
-            // book.id ===action.payload)
-            // if (index !== -1){
-            //     state.splice(index,1)
-            // }
-            return state.filter((book) => book.id !== action.payload);
-        },
-        toggleFavorite: (state, action) => {
-            //можно мутировать state так:
-            state.forEach((book) => {
-                if (book.id === action.payload) {
-                    book.isFavorite = !book.isFavorite;
-                }
-            });
-            // return state.map((book) =>
-            //     book.id === action.payload
-            //         ? { ...book, isFavorite: !book.isFavorite }
-            //         : book
-            // );
-        },
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      // return [...state, action.id];
+      state.push(action.payload);
     },
+    deleteBook: (state, action) => {
+      // то же самое, но много текста
+      // const index = state.findIndex((book)=>
+      // book.id ===action.payload)
+      // if (index !== -1){
+      //     state.splice(index,1)
+      // }
+      return state.filter((book) => book.id !== action.payload);
+    },
+    toggleFavorite: (state, action) => {
+      //можно мутировать state так:
+      state.forEach((book) => {
+        if (book.id === action.payload) {
+          book.isFavorite = !book.isFavorite;
+        }
+      });
+      // return state.map((book) =>
+      //     book.id === action.payload
+      //         ? { ...book, isFavorite: !book.isFavorite }
+      //         : book
+      // );
+    },
+  },
 });
+
+export const thunkFunction = async (dispatch, getState) => {
+  try {
+    const res = await axios.get('http://localhost:4000/random-book');
+    if (res?.data?.title && res?.data?.author) {
+      dispatch(addBook(createBookWithID(res.data, 'API')));
+    }
+  } catch (error) {
+    console.log('Error fetching random book', error);
+  }
+};
+
 export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
 export const selectBooks = (state) => state.books;
 export default booksSlice.reducer;
